886b1a6a6c7935e6cdbcb37b386ccc2fad6d9a97
Devrishi Sikka <devrishisikka@gmail.com>
2023-08-05 17:45:05 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,22 @@
+# -----------------------------------
+
+# Name: Devrishi Sikka
+# API Link: http://127.0.0.1:8000/redoc
+
+# -----------------------------------
+
+from routers import CategoryOperations, SKUOperations, Transactions
+from fastapi import FastAPI
+
+app = FastAPI()
+
+# Including all the paths to the main file
+app.include_router(Transactions.router)
+app.include_router(SKUOperations.router)
+app.include_router(CategoryOperations.router)
+
+
+# This is a dummy operation to test if the API is running or not
+@app.get('/')
+def Ping():
+    return {"message" : "Welcome"}
